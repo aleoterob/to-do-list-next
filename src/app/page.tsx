6d8616d8ef92cb5162a4db0a3636d3f550e9c713
Alejandro Otero <aleoterob@gmail.com>
2025-01-16 23:30:04 -0300

    Updates css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ const MyApp = () => {
         <h1 className="flex flex-row justify-center items-center w-full text-center sm:pl-0 md:pl-0 lg:pl-0 xl:pl-0 pt-6 text-2xl sm:text-4xl md:text-4xl lg:text-4xl xl:text-4xl  font-montserrat font-extralight text-white">
           Next.js - Redux Persist - Tailwind - Gsap.js
         </h1> */}
-        <div className="flex flex-col justify-center items-center w-full pt-10 pb-40">
+        <div className="flex flex-col justify-center items-center w-full min-h-screen">
           <TodoList />
         </div>
       </div>
